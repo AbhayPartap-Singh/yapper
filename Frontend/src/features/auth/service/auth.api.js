aaa9dfd160acba750ea6396fe764e95429acc9ff
AbhayPartap-Singh <partapabhay613@gmail.com>
2026-04-10 14:49:46 +0530

    typo

--- Frontend/src/features/auth/service/auth.api.js
+++ Frontend/src/features/auth/service/auth.api.js
@@ -6,17 +6,17 @@ const api = axios.create({
 })
 export async function register({email,username,password})
 {
-    const response = await api.post("api/auth/register",{email,username,password})
+    const response = await api.post("/auth/register",{email,username,password})
     return response.data
 }
 export async function login({email,password})
 {
-    const response = await api.post("api/auth/login",{email,password})
+    const response = await api.post("/auth/login",{email,password})
     return response.data
 }
 
 export async function getMe()
 {
-    const response = await api.get("api/auth/get-me")
+    const response = await api.get("/auth/get-me")
     return response.data
 }
